762c0ea24d14b394880d714f1106741946f05339
emilywilbur <ewilbur94@gmail.com>
2024-03-26 18:35:59 -0400

    reverted prior to new code

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import Img1 from '../assets/images/noods.png';
 import Img2 from '../assets/images/myrtle.png';
@@ -12,63 +12,96 @@ import Img9 from '../assets/images/alert.jpg';
 import Img10 from '../assets/images/rarebreed.png';
 import Img11 from '../assets/images/litner.png';
 
-export default function Gallery() {
-    const data = [
-        { id: 11, imgSrc: Img11 },
-        { id: 10, imgSrc: Img10 },
-        { id: 9, imgSrc: Img9 },
-        { id: 8, imgSrc: Img8 },
-        { id: 6, imgSrc: Img6 },
-        { id: 7, imgSrc: Img7 },
-        { id: 1, imgSrc: Img1 },
-        { id: 2, imgSrc: Img2 },
-        { id: 3, imgSrc: Img3 },
-        { id: 4, imgSrc: Img4 },
-        { id: 5, imgSrc: Img5 },
-    ];
 
+
+export default function Gallery(){
+
+    let data = [
+        {
+
+            id: 11,
+            imgSrc: Img11,
+        },
+        {
+
+            id: 10,
+            imgSrc: Img10,
+        },
+        {
+
+            id: 9,
+            imgSrc: Img9,
+        },
+        {
+
+            id: 8,
+            imgSrc: Img8,
+        },
+        {
+
+            id: 6,
+            imgSrc: Img6,
+        },
+        {
+            id: 7,
+            imgSrc:Img7,
+        },
+        {
+
+            id: 1,
+            imgSrc: Img1,
+        },
+        {
+
+            id: 2,
+            imgSrc: Img2,
+        },
+        {
+
+            id: 3,
+            imgSrc: Img3,
+        },
+        {
+
+            id: 4,
+            imgSrc: Img4,
+        },
+        {
+
+            id: 5,
+            imgSrc: Img5,
+        },
+    
+     
+       
+    ]
     const [model, setModel] = useState(false);
     const [tempimgSrc, setTempImgSrc] = useState('');
 
     const getImg = (imgSrc) => {
         setTempImgSrc(imgSrc);
         setModel(true);
-    };
+    }
+    return(
+       <>
+      <div className={model? "model open" : "model"}>
+        <img src={tempimgSrc} alt='img' />
+        <CloseIcon onClick={() => setModel(false)} />
+       
 
-    const galleryRef = useRef(null);
+    </div>      
+    <div className='gallery'>
+        {data.map((item, index) =>{
+            return(
+                <div className='pics' key={index} onClick={() => getImg(item.imgSrc)}>
+                    <img src={item.imgSrc} alt='photos' style={{width:'100%'}} />
+                </div>
+            )
+        })}
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const galleryItems = galleryRef.current.querySelectorAll('.pics');
-            galleryItems.forEach((item, index) => {
-                const rect = item.getBoundingClientRect();
-                if (rect.top < window.innerHeight) {
-                    item.style.transition = `opacity 0.5s ease ${index * 100}ms`;
-                    item.style.opacity = 1;
-                }
-            });
-        };
+      </div>
+       
+       </>
+    )
 
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    return (
-        <>
-            <div className={model ? 'model open' : 'model'}>
-                <img src={tempimgSrc} alt="img" />
-                <CloseIcon onClick={() => setModel(false)} />
-            </div>
-            <div ref={galleryRef} className="gallery">
-                {data.map((item, index) => (
-                    <div className="pics" key={index} onClick={() => getImg(item.imgSrc)}>
-                        <img src={item.imgSrc} alt="photos" style={{ width: '100%' }} />
-                    </div>
-                ))}
-            </div>
-        </>
-    );
 }
